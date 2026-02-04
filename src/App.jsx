0fb318d96903c1933b9c3ea64c2d94ffc1c9a178
Kanishk19092003 <kanishkyadav141@gmail.com>
2026-02-04 20:10:49 +0530

    Intialized authentication and authorization using firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { Routes,Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
+// pages
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
 function App() {
 
   return (
     <Routes>
       <Route path='/' element={<h1>Home</h1>} />
-      <Route path='/login' element={<h1>Login</h1>} />
+      <Route path='/login' element={<Login/>} />
+      <Route path='/register' element={<Register/>} />
 
     </Routes>
   )
--- src/Context/Firebase.jsx
+++ src/Context/Firebase.jsx
@@ -1,5 +1,15 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext,useState,useEffect } from "react";
 import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+  onAuthStateChanged,
+} from "firebase/auth";
+
+//creating context
 const FirebaseContext = createContext(null);
 
 const firebaseConfig = {
@@ -11,10 +21,54 @@ const firebaseConfig = {
   appId: "1:432545949713:web:5dceec0ffb6b5d7a5269b0",
 };
 
+//custom hook
 export const useFirebase = () => useContext(FirebaseContext);
 
+//intialize firebase (step 1)
 const firebaseApp = initializeApp(firebaseConfig);
+//firebase authentication using email and password
+const firebaseAuth = getAuth(firebaseApp);
+//google authentication
+const googleProvider = new GoogleAuthProvider();
 
+//firebase provider for all the pages and components
 export const FirebaseProvider = (props) => {
-  return <FirebaseContext.Provider>{props.children}</FirebaseContext.Provider>;
+    
+    //check user login or not 
+    const [user,setUser] = useState(null);
+    useEffect(()=>{
+        onAuthStateChanged(firebaseAuth ,(user)=>{
+            if(user) setUser(user);
+            else setUser(null);
+        })
+    },[])
+
+  //signUp
+  const signupWithEmailAndPassword = (email, password) => {
+    createUserWithEmailAndPassword(firebaseAuth, email, password);
+  };
+  
+  //signIn
+  const singinUserWithEmailAndPassword = (email, password) => {
+    signInWithEmailAndPassword(firebaseAuth, email, password);
+  };
+
+  //signup wihr google
+  const signupWithGoogle = () => signInWithPopup(firebaseAuth, googleProvider);
+
+  //checking user
+  const isLoggedIn = user ? true : false;
+
+  return (
+    <FirebaseContext.Provider
+      value={{
+        signupWithGoogle,
+        signupWithEmailAndPassword,
+        singinUserWithEmailAndPassword,
+        isLoggedIn
+      }}
+    >
+      {props.children}
+    </FirebaseContext.Provider>
+  );
 };
